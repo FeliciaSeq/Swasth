81abffa6657b253eba80be03873f0ab74dbb9a7d
FeliciaSeq <113540077+FeliciaSeq@users.noreply.github.com>
2025-04-22 17:55:14 -0400

    Update App.js

--- final-project-faug/client/src/App.js
+++ final-project-faug/client/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./App.css";
 import { Toaster } from "react-hot-toast";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import PublicRoute from './Components/PublicRoute.js';
 import PrivateRoute from './Components/PrivateRoute.js';
+
 import Login from './UserAccount/Login.js';
 import Signup from './UserAccount/Signup.js';
 import Home from './home/Home.js';
@@ -16,27 +17,34 @@ import TrainerSession from "./scheduler/Trainer/TrainerSession";
 import BookAppointment from "./scheduler/BookAppointment";
 import Appointments from "./scheduler/Appointments.js";
 
-
-//browse different routes according to the route path 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Toaster position="top-center" reverseOrder={false} />
         <Routes>
-          {/* Public routes are not authenticated */}
-          <Route path="user/login" element={<PublicRoute><Login /></PublicRoute>} />
-          <Route path="user/signup" element={<PublicRoute><Signup /></PublicRoute>} />
-          {/* Private routes are authenticated by session token */}
-          <Route path="home" element={<PrivateRoute><Home /></PrivateRoute>} />
-          <Route path="apply-trainer" element={<PrivateRoute><ApplyTrainer /></PrivateRoute>} />
-          <Route path="admin/trainerslist" element={<PrivateRoute><TrainersList /></PrivateRoute>} />
-          <Route path="admin/userslist" element={<PrivateRoute><UsersList /></PrivateRoute>} />
-          <Route path="find-trainers" element={<PrivateRoute><FindTrainers /></PrivateRoute>} />
-          <Route path="trainer/profile/:userId" element={<PrivateRoute><TrianerProfile /></PrivateRoute>} />
-          <Route path="trainer/sessions" element={<PrivateRoute><TrainerSession /></PrivateRoute>} />
-          <Route path="book-appointment/:trainerId" element={<PrivateRoute><BookAppointment /></PrivateRoute>} />
-          <Route path="appt/get-appointments-by-userId" element={<PrivateRoute><Appointments /></PrivateRoute>} />
+
+          {/* Default route redirects to login */}
+          <Route path="/" element={<Navigate to="/user/login" />} />
+
+          {/* Public routes */}
+          <Route path="/user/login" element={<PublicRoute><Login /></PublicRoute>} />
+          <Route path="/user/signup" element={<PublicRoute><Signup /></PublicRoute>} />
+
+          {/* Private routes */}
+          <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
+          <Route path="/apply-trainer" element={<PrivateRoute><ApplyTrainer /></PrivateRoute>} />
+          <Route path="/admin/trainerslist" element={<PrivateRoute><TrainersList /></PrivateRoute>} />
+          <Route path="/admin/userslist" element={<PrivateRoute><UsersList /></PrivateRoute>} />
+          <Route path="/find-trainers" element={<PrivateRoute><FindTrainers /></PrivateRoute>} />
+          <Route path="/trainer/profile/:userId" element={<PrivateRoute><TrianerProfile /></PrivateRoute>} />
+          <Route path="/trainer/sessions" element={<PrivateRoute><TrainerSession /></PrivateRoute>} />
+          <Route path="/book-appointment/:trainerId" element={<PrivateRoute><BookAppointment /></PrivateRoute>} />
+          <Route path="/appt/get-appointments-by-userId" element={<PrivateRoute><Appointments /></PrivateRoute>} />
+
+          {/* Catch-all route for 404 */}
+          <Route path="*" element={<Navigate to="/user/login" />} />
+          
         </Routes>
       </BrowserRouter>
     </div>
